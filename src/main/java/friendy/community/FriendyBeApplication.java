36bf5c55b5229e67e7199eb035e17ee892743a5a
Seio924 <seio9241@gmail.com>
2024-12-02 23:00:07 +0900

    chore(#1): 초기환경 세팅

---
+++ src/main/java/friendy/community/FriendyBeApplication.java
@@ -0,0 +1,13 @@
+package friendy.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendyBeApplication.class, args);
+	}
+
+}
