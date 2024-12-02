a08cc9c9bfa778d233e21a925711ac9ad6a56b8c
Seio924 <seio9241@gmail.com>
2024-12-02 23:00:07 +0900

    chore(#1): 초기환경 세팅

---
+++ src/test/java/friendy/community/FriendyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package friendy.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
