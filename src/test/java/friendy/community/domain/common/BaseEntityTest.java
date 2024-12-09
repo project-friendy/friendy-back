73460f626aec75721496cb49384cbcc0a93fb339
Seio924 <seio9241@gmail.com>
2024-12-09 21:26:57 +0900

    test(#16): 커버리지에서 제외할 파일 추가

---
+++ src/test/java/friendy/community/domain/common/BaseEntityTest.java
@@ -0,0 +1,16 @@
+package friendy.community.domain.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseEntityTest {
+
+    @Test
+    void getCreatedDate() {
+    }
+
+    @Test
+    void getModifiedDate() {
+    }
+}
