ae205724ca3b7c320960c398326ca5cda91a8716
Seio924 <seio9241@gmail.com>
2024-12-09 22:04:53 +0900

    test(#16): MemberTest로 ci 테스트

--- src/main/java/friendy/community/domain/member/model/Member.java
+++ src/main/java/friendy/community/domain/member/model/Member.java
@@ -1,16 +1,24 @@
 package friendy.community.domain.member.model;
 
-import jakarta.persistence.Embedded;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Embedded
+    @Column(nullable = false, unique = true)
     private String email;
 }
