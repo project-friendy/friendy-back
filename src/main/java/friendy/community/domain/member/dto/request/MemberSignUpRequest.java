117536374c510fc00f1dd34b777c303fb93dd5b0
Seio924 <seio9241@gmail.com>
2024-12-29 00:17:03 +0900

    feat(#18): 회원가입 validation 추가

--- src/main/java/friendy/community/domain/member/dto/request/MemberSignUpRequest.java
+++ src/main/java/friendy/community/domain/member/dto/request/MemberSignUpRequest.java
@@ -1,28 +1,31 @@
 package friendy.community.domain.member.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
 @Schema(description = "사용자 회원가입")
 public record MemberSignUpRequest(
 
-        @Schema(description = "사용자 이메일", example = "example@friendy.com")
-        @NotBlank(message = "사용자 이메일이 필요합니다")
+        @Schema(description = "이메일", example = "example@friendy.com")
+        @NotBlank(message = "이메일이 입력되지 않았습니다.")
+        @Email(message = "이메일 형식으로 입력해주세요.")
         String email,
 
-        @Schema(description = "사용자 닉네임", example = "bokSungKim")
-        @NotBlank(message = "사용자 닉네임이 필요합니다")
+        @Schema(description = "닉네임", example = "bokSungKim")
+        @NotBlank(message = "닉네임이 입력되지 않았습니다.")
+        @Size(min = 2, max = 20, message = "닉네임은 2~20자 사이로 입력해주세요.")
         String nickname,
 
-        @Schema(description = "사용자 비밀번호", example = "passwordExample")
-        @NotBlank(message = "사용자 비밀번호가 필요합니다")
+        @Schema(description = "비밀번호. 영어와 숫자를 반드시 포함해야 합니다.", example = "passwordExample")
+        @NotBlank(message = "비밀번호가 입력되지 않았습니다.")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~!@#$%^&*?]).*$\n", message = "숫자, 영문자, 특수문자(~!@#$%^&*?)를 포함해야 합니다.")
+        @Size(min = 8, max = 16, message = "비밀번호는 8~16자 사이로 입력해주세요.")
         String password,
 
-        @Schema(description = "사용자 생일", example = "2002-08-13")
-        @NotNull(message = "사용자 생일이 필요합니다")
+        @Schema(description = "생년월일", example = "2002-08-13")
+        @NotNull(message = "생년월일이 입력되지 않았습니다.")
         LocalDate birthDate
 
     ) {
