3d9da229c966454b2ea859d1f0ac5d912e4ce773
Seio924 <seio9241@gmail.com>
2024-12-29 01:46:24 +0900

    feat(#18): 회원가입 시 비밀번호 암호화 로직 추가

--- src/main/java/friendy/community/domain/member/model/Member.java
+++ src/main/java/friendy/community/domain/member/model/Member.java
@@ -23,21 +23,26 @@ public class Member {
     private Email email;
 
     @Embedded
+    @Column(unique = true, nullable = false)
     private Nickname nickname;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false)
     private Password password;
+
+    @Column(nullable = false)
+    private String salt;
 
     private LocalDate birthDate;
 
-    private Member(final MemberSignUpRequest request) {
+    private Member(final MemberSignUpRequest request, final String encryptedPassword, final String salt) {
         this.email = new Email(request.email());
         this.nickname = new Nickname(request.nickname());
-        this.password = new Password(request.password());
+        this.password = new Password(encryptedPassword);
+        this.salt = salt;
         this.birthDate = request.birthDate();
     }
 
-    public static Member of(final MemberSignUpRequest request) {
-        return new Member(request);
+    public static Member of(final MemberSignUpRequest request, final String encryptedPassword, final String salt) {
+        return new Member(request, encryptedPassword, salt);
     }
 }
