c6d77bb72faff8f58f9a1207c323faffb7d4c9e9
Seio924 <seio9241@gmail.com>
2024-12-29 01:07:46 +0900

    feat(#18): SHA-256 기반 비밀번호 암호화 구현

---
+++ src/main/java/friendy/community/domain/member/encryption/SHA2PasswordEncryptor.java
@@ -0,0 +1,25 @@
+package friendy.community.domain.member.encryption;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class SHA2PasswordEncryptor implements PasswordEncryptor{
+
+    private final MessageDigest digest;
+
+    public SHA2PasswordEncryptor() {
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("암호화 알고리즘이 잘못 명시되었습니다.", e);
+        }
+    }
+
+    @Override
+    public String encrypt(String plainPassword, String salt) {
+        String passwordWithSalt = plainPassword + salt;
+        byte[] encryptByte = digest.digest(passwordWithSalt.getBytes());
+        return Base64.getEncoder().encodeToString(encryptByte);
+    }
+}
