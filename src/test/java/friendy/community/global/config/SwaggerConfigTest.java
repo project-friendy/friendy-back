c1933efb041367f873a29b1fe9a9c18a103eeafa
Seio924 <seio9241@gmail.com>
2024-12-29 21:13:22 +0900

    fix(#18): Swagger 설정 파일 이동으로 인한 경로 수정

--- src/test/java/friendy/community/global/config/SwaggerConfigTest.java
+++ src/test/java/friendy/community/global/config/SwaggerConfigTest.java
@@ -1,5 +1,6 @@
 package friendy.community.global.config;
 
+import friendy.community.global.swagger.SwaggerConfig;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
