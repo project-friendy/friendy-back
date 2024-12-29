dace7e35fc02fe7e71e766ca6072bc47e1f573d2
Seio924 <seio9241@gmail.com>
2024-12-29 21:09:57 +0900

    refactor(#18): @RequiredArgsConstructor 사용하여 자동 생성
    - @RequiredArgsConstructor를 사용하여 final 필드에 대한 생성자를 자동으로 생성
    - MemberController가 인터페이스를 구현하도록 수정 (SpringDocMemberController)
    - 회원가입 성공 후 반환되는 URI 경로 수정
    /members/ -> /users/

--- src/main/java/friendy/community/domain/member/controller/MemberController.java
+++ src/main/java/friendy/community/domain/member/controller/MemberController.java
@@ -3,27 +3,22 @@ package friendy.community.domain.member.controller;
 import friendy.community.domain.member.dto.request.MemberSignUpRequest;
 import friendy.community.domain.member.service.MemberService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
 @RestController
-@RequestMapping("/auth")
-public class MemberController {
+@RequiredArgsConstructor
+public class MemberController implements SpringDocMemberController{
 
     private final MemberService memberService;
 
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
-
     @PostMapping("/signup")
     public ResponseEntity<Void> signUp(@Valid @RequestBody MemberSignUpRequest request) {
-        return ResponseEntity.created(URI.create("/members/" + memberService.signUp(request))).build();
+        return ResponseEntity.created(URI.create("/users/" + memberService.signUp(request))).build();
     }
 }
