4970be44c3631e8ac0f004a7b85e10495c35baff
Seio924 <seio9241@gmail.com>
2024-12-30 21:50:12 +0900

    test(#18): 테스트를 위한 회원 fixture 생성

---
+++ src/test/java/friendy/community/domain/member/fixture/MemberFixture.java
@@ -0,0 +1,29 @@
+package friendy.community.domain.member.fixture;
+
+import friendy.community.domain.member.encryption.PasswordEncryptor;
+import friendy.community.domain.member.encryption.SHA2PasswordEncryptor;
+import friendy.community.domain.member.model.Member;
+
+import java.time.LocalDate;
+
+public class MemberFixture {
+
+    private static final PasswordEncryptor passwordEncryptor = new SHA2PasswordEncryptor();
+
+    public static Member memberFixture() {
+        String encrypted = passwordEncryptor.encrypt(getFixturePlainPassword(), "salt");
+        return new Member(
+                1L,
+                "example@friendy.com",
+                "bokSungKim",
+                encrypted,
+                "salt",
+                LocalDate.parse("2002-08-13")
+        );
+    }
+
+    public static String getFixturePlainPassword() {
+        return "password123!";
+    }
+
+}
