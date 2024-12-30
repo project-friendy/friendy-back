6b75607b0d846e8590a192ec1330c63052413e81
Seio924 <seio9241@gmail.com>
2024-12-30 21:53:22 +0900

    test(#18): 회원가입 서비스 테스트

---
+++ src/test/java/friendy/community/domain/member/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package friendy.community.domain.member.service;
+
+import friendy.community.domain.member.dto.request.MemberSignUpRequest;
+import friendy.community.domain.member.fixture.MemberFixture;
+import friendy.community.domain.member.model.Member;
+import friendy.community.domain.member.repository.MemberRepository;
+import friendy.community.global.exception.ErrorCode;
+import friendy.community.global.exception.FriendyException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원가입이 성공적으로 처리되면 회원 ID를 반환한다")
+    void signUpSuccessfullyReturnsMemberId() {
+        // Given
+        Member member = MemberFixture.memberFixture();
+        MemberSignUpRequest memberSignUpRequest = new MemberSignUpRequest(member.getEmail(), member.getNickname(), member.getPassword(), member.getBirthDate());
+
+        // When
+        Long savedId = memberService.signUp(memberSignUpRequest);
+
+        // Then
+        assertThat(savedId).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("닉네임이 중복되면 FriendyException을 던진다")
+    void throwsExceptionWhenDuplicateNickname() {
+        // Given
+        Member savedMember = memberRepository.save(MemberFixture.memberFixture());
+
+        // When & Then
+        assertThatThrownBy(() -> memberService.assertUniqueName(savedMember.getNickname()))
+                .isInstanceOf(FriendyException.class)
+                .hasMessageContaining("닉네임이 이미 존재합니다.")
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.DUPLICATE_NICKNAME);
+    }
+
+}
