7df6fc8006e3bd925dd4f7881e3bbd240cbfac01
Seio924 <seio9241@gmail.com>
2024-12-31 18:10:24 +0900

    fix(#20): cors 설정

---
+++ src/main/java/friendy/community/global/config/WebCorsConfig.java
@@ -0,0 +1,20 @@
+package friendy.community.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
