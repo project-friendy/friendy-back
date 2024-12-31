03aeed65da5915c3ba2e1a515b6a0a30cda3cfe3
Seio924 <seio9241@gmail.com>
2024-12-31 16:41:41 +0900

    refactor(#18): 암호화 알고리즘 예외처리 FriendyException으로 변경
    - QueryDSL 자동 생성 파일 테스트 커버리지에서 제외

--- src/main/java/friendy/community/global/exception/ErrorCode.java
+++ src/main/java/friendy/community/global/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     INVALID_REQUEST(1101, HttpStatus.BAD_REQUEST),
 
     DUPLICATE_EMAIL(1201, HttpStatus.CONFLICT),
-    DUPLICATE_NICKNAME(1202, HttpStatus.CONFLICT);
+    DUPLICATE_NICKNAME(1202, HttpStatus.CONFLICT),
+
+    INTERNAL_SERVER_ERROR(2000, HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final int code;
     private final HttpStatus httpStatus;
