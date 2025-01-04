c546f3260db25f306c14ca3310b295514a935a9f
Seio924 <seio9241@gmail.com>
2025-01-04 20:15:52 +0900

    feat(#23): 로그인 API 엔드포인트 구현

---
+++ src/main/java/friendy/community/domain/auth/controller/AuthController.java
@@ -0,0 +1,30 @@
+package friendy.community.domain.auth.controller;
+
+import friendy.community.domain.auth.dto.request.LoginRequest;
+import friendy.community.domain.auth.dto.response.LoginResponse;
+import friendy.community.domain.auth.service.AuthService;
+import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(
+            @Valid @RequestBody LoginRequest loginRequest
+    ) {
+        final LoginResponse response = authService.login(loginRequest);
+
+        return ResponseEntity.ok()
+                .header("Authorization", "Bearer " + response.accessToken())
+                .header("Authorization-Refresh", "Bearer " + response.refreshToken())
+                .build();
+    }
+}
