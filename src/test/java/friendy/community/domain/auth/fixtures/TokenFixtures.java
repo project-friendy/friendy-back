731cbfbb1cbb11f4048cce836c0d5b05345aec82
Seio924 <seio9241@gmail.com>
2025-01-04 15:50:09 +0900

    test(#24): 토큰 생성 및 검증 테스트

---
+++ src/test/java/friendy/community/domain/auth/fixtures/TokenFixtures.java
@@ -0,0 +1,10 @@
+package friendy.community.domain.auth.fixtures;
+
+public class TokenFixtures {
+
+    public static final String MALFORMED_JWT_TOKEN = "aabbcc";
+
+    public static final String EXPIRED_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV4YW1wbGVAZnJpZW5keS5jb20iLCJpYXQiOjE2MDAwMDAwMDAsImV4cCI6MTYwMDAwMDEwMH0.mqOL2LPVIqTlrjqAWElM5XsJMgTjxWsEpOkr0atIdKs";
+
+}
+
