f8202568b878d376e7e1adf75d420f7650804e5e
Seio924 <seio9241@gmail.com>
2025-01-04 20:00:30 +0900

    feat(#23): 로그인 서비스 구현

--- src/main/java/friendy/community/domain/member/repository/MemberRepository.java
+++ src/main/java/friendy/community/domain/member/repository/MemberRepository.java
@@ -4,10 +4,15 @@ import friendy.community.domain.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
 
     boolean existsByNickname(String nickname);
+
+    Optional<Member> findByEmail(String email);
+
 }
