e67258a539803b641b91400fc17a4bdf15422ad3
Seio924 <seio9241@gmail.com>
2025-01-05 16:23:32 +0900

    test(#27): 토큰 재발급 로직 테스트

--- src/test/java/friendy/community/domain/auth/fixtures/TokenFixtures.java
+++ src/test/java/friendy/community/domain/auth/fixtures/TokenFixtures.java
@@ -2,6 +2,11 @@ package friendy.community.domain.auth.fixtures;
 
 public class TokenFixtures {
 
+    /**
+     * CORRECT_REFRESH_TOKEN : 만료 기한 - 2123년 10월 30일
+     */
+    public static final String CORRECT_REFRESH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV4YW1wbGVAZnJpZW5keS5jb20iLCJpYXQiOjE2MDAwMDAwMDAsImV4cCI6NDg1NDI3ODQwMH0.I5Y8zaMf6ys1X3ESNzVK3HzH7mJFquPwiyAnmiH4RQg";
+
     public static final String MALFORMED_JWT_TOKEN = "aabbcc";
 
     public static final String EXPIRED_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV4YW1wbGVAZnJpZW5keS5jb20iLCJpYXQiOjE2MDAwMDAwMDAsImV4cCI6MTYwMDAwMDEwMH0.mqOL2LPVIqTlrjqAWElM5XsJMgTjxWsEpOkr0atIdKs";
