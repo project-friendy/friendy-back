cb19e85e36b3aea3400048779d0b075e9a5789c5
Seio924 <seio9241@gmail.com>
2025-01-12 19:35:18 +0900

    feat(#32): 인증코드 전송 API 엔드포인트 구현

---
+++ src/main/java/friendy/community/domain/email/controller/EmailController.java
@@ -0,0 +1,27 @@
+package friendy.community.domain.email.controller;
+
+import friendy.community.domain.email.dto.request.EmailRequest;
+import friendy.community.domain.email.dto.request.VerifyCodeRequest;
+import friendy.community.domain.email.service.EmailService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/send-code")
+    public ResponseEntity<Void> sendAuthenticatedEmail(@Valid @RequestBody EmailRequest request) {
+        emailService.sendAuthenticatedEmail(request);
+        return ResponseEntity.ok().build();
+    }
+
+}
