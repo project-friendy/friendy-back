d74c50a24caff069f052e14a5291f6eed8262828
jw <rlawodnjs353@naver.com>
2025-01-13 16:10:53 +0900

    feat(#34) 게시글 api 구현

--- src/main/java/friendy/community/domain/post/controller/PostController.java
+++ src/main/java/friendy/community/domain/post/controller/PostController.java
@@ -1,28 +1,34 @@
-//package friendy.community.domain.post.controller;
-//
-//import friendy.community.domain.post.dto.request.PostCreateRequest;
-//import friendy.community.domain.post.service.PostService;
-//import jakarta.validation.Valid;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequiredArgsConstructor
-//public class PostController implements SpringDocPostController {
-//
-//    private final PostService postService;
-//
-//    @PostMapping("/auth/posts/create/{memberId}")
-//    public ResponseEntity<Void> createPost(
-//            @PathVariable Long memberId,
-//            @Valid @RequestBody PostCreateRequest postCreateRequest
-//            ) {
-//        postService.createPost(postCreateRequest, memberId);
-//        return ResponseEntity.status(HttpStatus.CREATED).build();
-//    }
-//}
+package friendy.community.domain.post.controller;
+
+import friendy.community.domain.auth.jwt.JwtTokenExtractor;
+import friendy.community.domain.auth.jwt.JwtTokenProvider;
+import friendy.community.domain.post.dto.request.PostCreateRequest;
+import friendy.community.domain.post.service.PostService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController implements SpringDocPostController {
+
+    private final PostService postService;
+    private final JwtTokenExtractor jwtTokenExtractor;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/posts")
+    public ResponseEntity<Void> createPost(
+            HttpServletRequest httpServletRequest,
+            @Valid @RequestBody PostCreateRequest postCreateRequest
+            ) {
+
+        return ResponseEntity.created(URI.create("/posts/" + postService.savePost(postCreateRequest, jwtTokenProvider.extractEmailFromAccessToken(jwtTokenExtractor.extractAccessToken(httpServletRequest))))).build();
+
+    }
+}
