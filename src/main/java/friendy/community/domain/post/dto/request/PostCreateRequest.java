6080a5a2c25a037cbd6cc835f71c120ba652149c
jw <rlawodnjs353@naver.com>
2025-01-14 10:37:19 +0900

    feat(#34): PostCreateRequest에 제목 생성날짜 제거

--- src/main/java/friendy/community/domain/post/dto/request/PostCreateRequest.java
+++ src/main/java/friendy/community/domain/post/dto/request/PostCreateRequest.java
@@ -9,17 +9,9 @@ import java.time.LocalDate;
 @Schema(description = "게시판 생성")
 public record PostCreateRequest(
 
-        @Schema(description = "게시글 제목", example = "프렌디제목")
-        @NotBlank(message = "게시글 제목이 입력되지 않았습니다")
-        String title,
-
         @Schema(description = "게시글 내용", example = "프렌디게시글내용")
         @NotBlank(message = "게시글 내용이 입력되지 않았습니다")
-        String content,
-
-        @Schema(description = "생성날짜" ,example = "2024-08-13")
-        @NotNull(message = "생성날짜가 입력되지않았습니다")
-        LocalDate createdAt
+        String content
 
     ) {
 }
