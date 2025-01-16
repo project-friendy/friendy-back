9552ac12b2343b994ebf6ae89125529c91fce31d
jw <rlawodnjs353@naver.com>
2025-01-16 09:45:40 +0900

    feat(#34): Post객체 테스트코드 수정

--- src/test/java/friendy/community/domain/post/model/PostTest.java
+++ src/test/java/friendy/community/domain/post/model/PostTest.java
@@ -1,34 +1,29 @@
 package friendy.community.domain.post.model;
 
 import friendy.community.domain.member.dto.request.MemberSignUpRequest;
+import friendy.community.domain.member.fixture.MemberFixture;
 import friendy.community.domain.member.model.Member;
 import friendy.community.domain.post.dto.request.PostCreateRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest
 class PostTest {
 
     private Member member;
 
     @BeforeEach
     void setUp() {
-        MemberSignUpRequest signUpRequest = new MemberSignUpRequest(
-            "example@friendy.com",
-            "bokSungKim",
-            "password123!",
-            LocalDate.of(2002, 8, 13)
-        );
 
-        String encryptedPassword = "encryptedDummyPassword";
-        String salt = "dummySalt";
-        member = Member.of(signUpRequest,encryptedPassword,salt);
-
+        member = MemberFixture.memberFixture();
 
     }
 
@@ -39,12 +34,13 @@ class PostTest {
         String content = "This is a new post content.";
         PostCreateRequest postCreateRequest = new PostCreateRequest(content);
 
-        // when
+        // When
         Post post = Post.of(postCreateRequest, member);
 
-        // Assert
+        // Then
         assertNotNull(post);
         assertEquals(content, post.getContent());
         assertEquals(member, post.getMember());
+
     }
 }
