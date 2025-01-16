27f91bc3896c629e7be687017e7c9648a38be018
jw <rlawodnjs353@naver.com>
2025-01-16 17:18:41 +0900

    feat(#34):Application 쓸모없는 import제거

--- src/main/java/friendy/community/FriendyBeApplication.java
+++ src/main/java/friendy/community/FriendyBeApplication.java
@@ -2,13 +2,11 @@ package friendy.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class FriendyBeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FriendyBeApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(FriendyBeApplication.class, args);
-	}
-
+    }
 }
