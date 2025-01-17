78a0f32173baab6eefe8b63910a282c909a058f0
jw <rlawodnjs353@naver.com>
2025-01-17 09:22:43 +0900

    feat(#40): PostUpdateRequest dto 생성

--- src/main/java/friendy/community/domain/post/controller/PostController.java
+++ src/main/java/friendy/community/domain/post/controller/PostController.java
@@ -6,10 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -28,4 +25,5 @@ public class PostController implements SpringDocPostController {
         return ResponseEntity.created(URI.create("/posts/" + postId)).build();
 
     }
+
 }
