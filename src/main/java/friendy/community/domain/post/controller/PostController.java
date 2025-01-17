41f146189acaa4316b72413c96520eca293b07fa
jw <rlawodnjs353@naver.com>
2025-01-17 10:27:11 +0900

    feat(#40): Post수정 컨트롤러 작성

--- src/main/java/friendy/community/domain/post/controller/PostController.java
+++ src/main/java/friendy/community/domain/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package friendy.community.domain.post.controller;
 
 import friendy.community.domain.post.dto.request.PostCreateRequest;
+import friendy.community.domain.post.dto.request.PostUpdateRequest;
 import friendy.community.domain.post.service.PostService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -23,6 +24,18 @@ public class PostController implements SpringDocPostController {
         Long postId = postService.savePost(postCreateRequest, httpServletRequest);
 
         return ResponseEntity.created(URI.create("/posts/" + postId)).build();
+
+    }
+
+    @PostMapping("/{postId}")
+    public ResponseEntity<Void> modifyPost(
+        HttpServletRequest httpServletRequest,
+        @PathVariable Long postId,
+        @Valid @RequestBody PostUpdateRequest postUpdateRequest) {
+
+        long returnPostId = postService.updatePost(postUpdateRequest,httpServletRequest,postId);
+
+        return ResponseEntity.created(URI.create("/posts/" + returnPostId)).build();
 
     }
 
