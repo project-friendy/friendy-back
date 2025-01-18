7c4364948d740700d9f269bd12298b57f602b533
김재원 <rlawodnjs353@naver.com>
2025-01-18 22:31:52 +0900

    refactor(#40): TokenFixtures 변수명 수정

--- src/test/java/friendy/community/domain/auth/fixtures/TokenFixtures.java
+++ src/test/java/friendy/community/domain/auth/fixtures/TokenFixtures.java
@@ -5,7 +5,10 @@ public class TokenFixtures {
     /**
      * CORRECT_REFRESH_TOKEN : 만료 기한 - 2123년 10월 30일
      */
+    public static final String CORRECT_ACCESS_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV4YW1wbGVAZnJpZW5keS5jb20iLCJpYXQiOjE2MDAwMDAwMDAsImV4cCI6NDg1NDI3ODQwMH0.I5Y8zaMf6ys1X3ESNzVK3HzH7mJFquPwiyAnmiH4RQg";
     public static final String CORRECT_REFRESH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV4YW1wbGVAZnJpZW5keS5jb20iLCJpYXQiOjE2MDAwMDAwMDAsImV4cCI6NDg1NDI3ODQwMH0.I5Y8zaMf6ys1X3ESNzVK3HzH7mJFquPwiyAnmiH4RQg";
+
+    public static final String OTHER_USER_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAZXhhbXBsZS5jb20iLCJpYXQiOjE3MDcwNTI4MDAsImV4cCI6NDg1NDg4MzIwMH0.KBSErlIecTDvYFvmO0D0QQX1cnNeTf9KDgTGgOFY1f0";
 
     public static final String MALFORMED_JWT_TOKEN = "aabbcc";
 
