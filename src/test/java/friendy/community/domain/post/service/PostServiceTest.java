967f0e51d08c525f2c54d09aea78e67cb5cc7f50
김재원 <rlawodnjs353@naver.com>
2025-01-18 17:36:27 +0900

    refactor(#40): 공통 초기화 코드 추가

--- src/test/java/friendy/community/domain/post/service/PostServiceTest.java
+++ src/test/java/friendy/community/domain/post/service/PostServiceTest.java
@@ -14,12 +14,15 @@ import friendy.community.global.exception.ErrorCode;
 import friendy.community.global.exception.FriendyException;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.time.LocalDate;
 
 import static friendy.community.domain.auth.fixtures.TokenFixtures.CORRECT_REFRESH_TOKEN;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,15 +33,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 @DirtiesContext
 class PostServiceTest {
 
-
-
     @Autowired
     private PostService postService;
 
     @Autowired
     private MemberService memberService;
-
-    private Member member;
 
     @Autowired
     private MemberRepository memberRepository;
@@ -49,48 +48,45 @@ class PostServiceTest {
     @Autowired
     private EntityManager entityManager;
 
+    private Member member;
 
-    public void clean() {
+    private MockHttpServletRequest httpServletRequest;
+
+    @BeforeEach
+    void setup() {
+
+        httpServletRequest = new MockHttpServletRequest();
+        httpServletRequest.addHeader("Authorization", "Bearer " + CORRECT_REFRESH_TOKEN);
+
+        member = MemberFixture.memberFixture();
+        MemberSignUpRequest memberSignUpRequest = new MemberSignUpRequest(
+                member.getEmail(), member.getNickname(), member.getPassword(), member.getBirthDate()
+        );
+        memberService.signUp(memberSignUpRequest);
+
+        resetPostIdSequence();
+    }
+
+    private void resetPostIdSequence() {
         entityManager.createNativeQuery("ALTER TABLE post AUTO_INCREMENT = 1")
                 .executeUpdate();
     }
 
 
-    void signUpSetUp() {
-        member = MemberFixture.memberFixture();
-        MemberSignUpRequest memberSignUpRequest = new MemberSignUpRequest(member.getEmail(), member.getNickname(), member.getPassword(), member.getBirthDate());
+    private void postSetUp(String content) {
 
-        memberService.signUp(memberSignUpRequest);
-    }
-
-    void postSetUp() {
-        MockHttpServletRequest httpServletRequest = new MockHttpServletRequest();
-        String token = "Bearer " + CORRECT_REFRESH_TOKEN;
-        httpServletRequest.addHeader("Authorization", token);
-
-        String content = "This is a new post content.";
         PostCreateRequest postCreateRequest = new PostCreateRequest(content);
-
-        signUpSetUp();
-
         Long postId = postService.savePost(postCreateRequest, httpServletRequest);
-
     }
+
 
     @Test
     @DisplayName("게시글이 성공적으로 생성되면 게시글 ID를 반환한다")
-    @Transactional
     void createPostSuccessfullyReturnsPostId() {
 
         //Given
-        MockHttpServletRequest httpServletRequest = new MockHttpServletRequest();
-        String token = "Bearer " + CORRECT_REFRESH_TOKEN;
-        httpServletRequest.addHeader("Authorization", token);
-
         String content = "This is a new post content.";
         PostCreateRequest postCreateRequest = new PostCreateRequest(content);
-
-        signUpSetUp();
 
         //When
         Long postId = postService.savePost(postCreateRequest, httpServletRequest);
@@ -102,13 +98,10 @@ class PostServiceTest {
 
     @Test
     @DisplayName("이메일이 존재하지 않으면 FriendyException 던진다")
-    @Transactional
     void throwsExceptionWhenEmailNotFound() {
         // Given
-        MockHttpServletRequest httpServletRequest = new MockHttpServletRequest();
-        String token = "Bearer " + CORRECT_REFRESH_TOKEN;
-        httpServletRequest.addHeader("Authorization", token);
 
+        memberRepository.deleteAll();
         String content = "This is a new post content.";
         PostCreateRequest postCreateRequest = new PostCreateRequest(content);
 
@@ -126,23 +119,16 @@ class PostServiceTest {
 
     @Test
     @DisplayName("게시글이 성공적으로 수정되면 게시글 id를 반환한다")
-    @Transactional
     void updatePostSuccessfullyReturnsPostId() {
 
         //Given
-        clean();
-        MockHttpServletRequest httpServletRequest = new MockHttpServletRequest();
-        String token = "Bearer " + CORRECT_REFRESH_TOKEN;
-        httpServletRequest.addHeader("Authorization", token);
-
         String updateContent = "Update content";
         PostUpdateRequest postUpdateRequest = new PostUpdateRequest(updateContent);
 
-        postSetUp();
-
+        postSetUp("This is content");
 
         Post post = postRepository.findById(1L)
-            .orElseThrow(() -> new FriendyException(ErrorCode.POST_NOT_FOUND, "존재하지 않는 게시글입니다"));
+            .orElseThrow(() -> new FriendyException(ErrorCode.RESOURCE_NOT_FOUND, "존재하지 않는 게시글입니다"));
 
         //When
         Long postId = postService.updatePost(postUpdateRequest, httpServletRequest ,1L);
@@ -156,20 +142,41 @@ class PostServiceTest {
     @DisplayName("존재하지 않는 게시물을 수정하면 FriendyException을 던진다")
     void throwsExceptionWhenPostNotFound() {
         // Given
-
-        MockHttpServletRequest httpServletRequest = new MockHttpServletRequest();
-        String token = "Bearer " + CORRECT_REFRESH_TOKEN;
-        httpServletRequest.addHeader("Authorization", token);
-
         String updateContent = "Update content";
         PostUpdateRequest postUpdateRequest = new PostUpdateRequest(updateContent);
 
-        postSetUp();
+        postSetUp("This is content");
 
         // When & Then
         assertThatThrownBy(() -> postService.updatePost(postUpdateRequest,httpServletRequest,999L))
                 .isInstanceOf(FriendyException.class)
                 .hasMessageContaining("존재하지 않는 게시글입니다")
-                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.POST_NOT_FOUND);
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.RESOURCE_NOT_FOUND);
     }
+
+    @Test
+    @DisplayName("게시글 작성자가 아닌 사용자가 수정하면 FriendyException을 던진다")
+    void throwsExceptionWhenNotPostAuthor() {
+        // Given
+        String updateContent = "Update content";
+        PostUpdateRequest postUpdateRequest = new PostUpdateRequest(updateContent);
+        postSetUp("This is content");
+
+
+        MemberSignUpRequest memberSignUpRequest = new MemberSignUpRequest(
+                "email2.friendy.com", "홍길동", "password123!", LocalDate.parse("2002-08-13")
+        );
+        memberService.signUp(memberSignUpRequest);
+
+        httpServletRequest = new MockHttpServletRequest();
+        httpServletRequest.addHeader("Authorization", "Bearer " +
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYWlsMi5mcmllbmR5LmNvbSIsImlhdCI6MTYwMDAwMDAwMCwiZXhwIjo0ODU0Mjc4NDAwfQ.oljB25ve-TyrKcvbtTUoaFKkpQeVTwVwy1p1abH0IUU");
+
+        // When & Then
+        assertThatThrownBy(() -> postService.updatePost(postUpdateRequest, httpServletRequest, 1L)) // 1L: 존재하는 게시글
+                .isInstanceOf(FriendyException.class) // 예외 타입 확인
+                .hasMessageContaining("게시글 작성자가 아닙니다") // 예외 메시지 확인
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.FORBIDDEN_ACCESS); // 에러 코드 확인
+    }
+
 }
