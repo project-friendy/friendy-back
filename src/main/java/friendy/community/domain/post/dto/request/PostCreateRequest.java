5439bd2677be10bd040fffff9ec467ca97c5f962
김재원 <rlawodnjs353@naver.com>
2025-01-20 21:14:15 +0900

    refactor(#44): 에러메세지 수정

--- src/main/java/friendy/community/domain/post/dto/request/PostCreateRequest.java
+++ src/main/java/friendy/community/domain/post/dto/request/PostCreateRequest.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.*;
 public record PostCreateRequest(
 
         @Schema(description = "게시글 내용", example = "프렌디게시글내용")
-        @NotBlank(message = "게시글 내용이 입력되지 않았습니다")
+        @NotBlank(message = "게시글 내용이 입력되지 않았습니다.")
         @Size(max = 2200, message = "게시글은 2200자 이내로 작성해주세요.")
         String content
 
