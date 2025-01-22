2772756180782586e72e8d6e1b724220c1cc5adc
김재원 <rlawodnjs353@naver.com>
2025-01-22 19:43:30 +0900

    feat(#46): 게시글 목록 조회 컨트롤러 구현

--- src/main/java/friendy/community/domain/post/controller/PostController.java
+++ src/main/java/friendy/community/domain/post/controller/PostController.java
@@ -2,13 +2,16 @@ package friendy.community.domain.post.controller;
 
 import friendy.community.domain.post.dto.request.PostCreateRequest;
 import friendy.community.domain.post.dto.request.PostUpdateRequest;
+import friendy.community.domain.post.dto.response.PostListResponse;
 import friendy.community.domain.post.service.PostService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.net.URI;
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequiredArgsConstructor
@@ -40,12 +43,19 @@ public class PostController implements SpringDocPostController {
 
     @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(
-        HttpServletRequest httpServletRequest,
-        @PathVariable Long postId) {
+            HttpServletRequest httpServletRequest,
+            @PathVariable Long postId) {
 
         postService.deletePost(httpServletRequest, postId);
 
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/list")
+    public ResponseEntity<PostListResponse> getPostsList(
+            @RequestParam(defaultValue = "0") int page
+    ) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return ResponseEntity.ok(postService.getAllPosts(pageable));
+    }
 }
