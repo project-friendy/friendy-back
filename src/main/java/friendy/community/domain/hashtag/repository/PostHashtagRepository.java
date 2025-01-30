9c6a5eb5d3a4ea18f48d224e8f9b1700a339faa0
Seio924 <seio9241@gmail.com>
2025-01-30 20:26:15 +0900

    feat(#56): 해시태그 삭제 로직 추가

--- src/main/java/friendy/community/domain/hashtag/repository/PostHashtagRepository.java
+++ src/main/java/friendy/community/domain/hashtag/repository/PostHashtagRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostHashtagRepository extends JpaRepository<PostHashtag, Long> {
+
+    void deleteAllByPostId(Long postId);
 }
