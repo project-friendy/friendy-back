879043d3ed708e04efe73e5af7ab2ee80337a04f
Seio924 <seio9241@gmail.com>
2025-01-30 19:30:15 +0900

    feat(#56): 해시태그 저장 로직 추가

---
+++ src/main/java/friendy/community/domain/hashtag/service/HashtagService.java
@@ -0,0 +1,54 @@
+package friendy.community.domain.hashtag.service;
+
+import friendy.community.domain.hashtag.model.Hashtag;
+import friendy.community.domain.hashtag.repository.HashtagRepository;
+import friendy.community.domain.hashtag.repository.PostHashtagRepository;
+import friendy.community.domain.post.model.Post;
+import friendy.community.domain.post.model.PostHashtag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class HashtagService {
+
+    private final HashtagRepository hashtagRepository;
+    private final PostHashtagRepository postHashtagRepository;
+
+    public void saveHashtags(Post post, List<String> hashtagNames) {
+        List<Hashtag> existTags = hashtagRepository.findAllByNameIn(hashtagNames);
+        List<String> existNames = getExistHashtagNames(existTags);
+
+        List<Hashtag> newHashtags = getOnlyNewHashags(existNames, hashtagNames);
+        List<Hashtag> savedNewHashtags = hashtagRepository.saveAll(newHashtags);
+
+        existTags.addAll(savedNewHashtags);
+        savePostHashtags(post, existTags);
+    }
+
+    private List<String> getExistHashtagNames(List<Hashtag> existTags) {
+        return existTags.stream()
+                .map(Hashtag::getName)
+                .toList();
+    }
+
+    private List<Hashtag> getOnlyNewHashags(List<String> existNames, List<String> hashtagNames) {
+        return hashtagNames.stream()
+                .distinct()
+                .filter(name -> !existNames.contains(name))
+                .map(Hashtag::new)
+                .toList();
+    }
+
+    private void savePostHashtags(Post post, List<Hashtag> hashtags) {
+        List<PostHashtag> postHashtags = hashtags.stream()
+                .map(hashtag -> new PostHashtag(post, hashtag))
+                .toList();
+        postHashtagRepository.saveAll(postHashtags);
+    }
+
+}
