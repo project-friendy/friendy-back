d343c8a93f2dd9919497c2deca5869d12a0f107a
Seio924 <seio9241@gmail.com>
2025-01-30 20:51:23 +0900

    feat(#56): request에 해시태그 추가 및 해시태그 서비스 추가

--- src/main/java/friendy/community/domain/post/dto/request/PostUpdateRequest.java
+++ src/main/java/friendy/community/domain/post/dto/request/PostUpdateRequest.java
@@ -3,11 +3,17 @@ package friendy.community.domain.post.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 
+import java.util.List;
+
 @Schema(description = "게시글 수정")
 public record PostUpdateRequest(
-    @Schema(description = "게시글 내용", example = "프렌디게시글내용업데이트")
-    @NotBlank(message = "게시글 내용이 입력되지 않았습니다")
-    @Size(max = 2200, message = "게시글은 2200자 이내로 작성해주세요.")
-    String content
+
+        @Schema(description = "게시글 내용", example = "프렌디게시글내용")
+        @NotBlank(message = "게시글 내용이 입력되지 않았습니다.")
+        @Size(max = 2200, message = "게시글은 2200자 이내로 작성해주세요.")
+        String content,
+
+        @Schema(description = "게시글 해시태그", example = "[\"프렌디\", \"개발\", \"스터디\"]")
+        List<String> hashtags
 ) {
 }
