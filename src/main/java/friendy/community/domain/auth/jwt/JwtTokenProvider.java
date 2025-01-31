e2b1ed80b99375caf186e42e4ba97772374dc815
Wooki <hyunook02@naver.com>
2025-01-31 19:59:11 +0900

    fix(#50): 매개변수 제거

--- src/main/java/friendy/community/domain/auth/jwt/JwtTokenProvider.java
+++ src/main/java/friendy/community/domain/auth/jwt/JwtTokenProvider.java
@@ -66,7 +66,7 @@ public class JwtTokenProvider {
             final String logMessage = "인증 실패(JWT 리프레시 토큰 Payload 이메일 누락) - 토큰 : " + token;
             throw new FriendyException(ErrorCode.UNAUTHORIZED_USER, logMessage);
         }
-        validateRefreshTokenInRedis(token, extractedEmail);
+        validateRefreshTokenInRedis(extractedEmail);
         return extractedEmail;
     }
 
