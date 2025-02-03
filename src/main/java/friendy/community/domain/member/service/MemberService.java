2b1f71a368d3e50368528929174337e01d3c34bd
jw <rlawodnjs353@naver.com>
2025-02-03 17:41:10 +0900

    refactor(#54): 회원가입 서비스 로직 수정

--- src/main/java/friendy/community/domain/member/service/MemberService.java
+++ src/main/java/friendy/community/domain/member/service/MemberService.java
@@ -1,17 +1,20 @@
 package friendy.community.domain.member.service;
 
+import friendy.community.domain.common.S3service;
 import friendy.community.domain.member.dto.request.PasswordRequest;
 import friendy.community.domain.auth.service.AuthService;
 import friendy.community.domain.member.dto.request.MemberSignUpRequest;
 import friendy.community.domain.member.encryption.PasswordEncryptor;
 import friendy.community.domain.member.encryption.SaltGenerator;
 import friendy.community.domain.member.model.Member;
+import friendy.community.domain.member.model.MemberImage;
 import friendy.community.domain.member.repository.MemberRepository;
 import friendy.community.global.exception.ErrorCode;
 import friendy.community.global.exception.FriendyException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -22,12 +25,26 @@ public class MemberService {
     private final SaltGenerator saltGenerator;
     private final PasswordEncryptor passwordEncryptor;
     private final AuthService authService;
+    private final S3service s3service;
 
-    public Long signUp(MemberSignUpRequest request) {
+    public Long signUp(MemberSignUpRequest request, MultipartFile image) {
         validateUniqueMemberAttributes(request);
         final String salt = saltGenerator.generate();
         final String encryptedPassword = passwordEncryptor.encrypt(request.password(), salt);
-        final Member member = Member.of(request, encryptedPassword, salt);
+        String imageUrl = "default-profile-image-url";
+        Member member = new Member(request, encryptedPassword, salt);
+
+        if (image != null && !image.isEmpty()) {
+            try {
+                imageUrl = s3service.upload(image, "profile");
+                String storedFileName = s3service.generateStoredFileName(image);
+                String fileType = s3service.getFileType(image);
+                final MemberImage memberImage = MemberImage.of(member, imageUrl, storedFileName, fileType);
+                member.setMemberImage(memberImage);
+            } catch (Exception e) {
+                throw new RuntimeException("이미지 업로드 실패", e);
+            }
+        }
         memberRepository.save(member);
 
         return member.getId();
