9c63758983f8905c8de4d384a72a3f830cb33e74
jw <rlawodnjs353@naver.com>
2025-02-03 17:39:46 +0900

    #refactor(#54): 회원가입 컨트롤러 수정

--- src/main/java/friendy/community/domain/member/controller/MemberController.java
+++ src/main/java/friendy/community/domain/member/controller/MemberController.java
@@ -5,10 +5,10 @@ import friendy.community.domain.member.dto.request.MemberSignUpRequest;
 import friendy.community.domain.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URI;
 
@@ -18,9 +18,10 @@ public class MemberController implements SpringDocMemberController{
 
     private final MemberService memberService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<Void> signUp(@Valid @RequestBody MemberSignUpRequest request) {
-        return ResponseEntity.created(URI.create("/users/" + memberService.signUp(request))).build();
+    @PostMapping(value = "/signup", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Void> signUp(@RequestPart(value = "request") MemberSignUpRequest request,
+                                       @RequestPart(value = "file") MultipartFile multipartFile) {
+        return ResponseEntity.created(URI.create("/users/" + memberService.signUp(request,multipartFile))).build();
     }
 
     @PostMapping("/password")
