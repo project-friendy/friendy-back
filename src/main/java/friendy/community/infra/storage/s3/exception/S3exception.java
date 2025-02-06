fa303a67c06f94c706dca2ac136bbc7447741b8b
jw <rlawodnjs353@naver.com>
2025-02-06 17:39:17 +0900

    feat(#54): S3exception 구현

---
+++ src/main/java/friendy/community/infra/storage/s3/exception/S3exception.java
@@ -0,0 +1,68 @@
+package friendy.community.infra.storage.s3.exception;
+
+import friendy.community.global.exception.ErrorCode;
+import friendy.community.global.exception.FriendyException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class S3exception {
+
+    private static final long MAX_FILE_SIZE = 1024 * 1024;
+
+    public void validateFile(MultipartFile multipartFile) {
+        validateEmptyFile(multipartFile);
+        validateFileExtension(multipartFile);
+        validateMimeType(multipartFile);
+        validateFileSize(multipartFile);
+    }
+
+    private void validateMimeType(MultipartFile multipartFile) {
+        String contentType = multipartFile.getContentType();
+        if (contentType != null && !isAllowedMimeType(contentType)) {
+            throw new FriendyException(ErrorCode.INVALID_FILE, "지원되지 않는 파일 확장자입니다.");
+        }
+    }
+
+    private void validateEmptyFile(MultipartFile multipartFile) {
+        if (multipartFile.isEmpty()) {
+            throw new FriendyException(ErrorCode.INVALID_FILE, "파일이 비어 있습니다.");
+        }
+    }
+    private void validateFileExtension(MultipartFile multipartFile) {
+        String fileName = multipartFile.getOriginalFilename();
+
+        // 파일 이름이 null인지 먼저 확인
+        if (fileName == null || fileName.isEmpty()) {
+            throw new FriendyException(ErrorCode.INVALID_FILE, "파일 이름이 없거나 확장자가 없습니다.");
+        }
+
+        if (!fileName.contains(".")) {
+            throw new FriendyException(ErrorCode.INVALID_FILE, "확장자가 없습니다.");
+        }
+
+        // 확장자 추출
+        String[] allowedExtensions = {"jpg", "png", "gif", "pdf"};
+        String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase(); // 확장자 추출
+
+        // 허용된 확장자 목록에 포함되지 않으면 예외 처리
+        if (!Arrays.asList(allowedExtensions).contains(fileExtension)) {
+            throw new FriendyException(ErrorCode.INVALID_FILE, "지원되지 않는 파일 확장자입니다.");
+        }
+    }
+
+
+    private void validateFileSize(MultipartFile multipartFile) {
+        if (multipartFile.getSize() > MAX_FILE_SIZE) {
+            throw new FriendyException(ErrorCode.INVALID_FILE, "파일 크기가 허용된 범위를 초과했습니다.");
+        }
+    }
+
+    private boolean isAllowedMimeType(String contentType) {
+        return List.of("image/jpeg", "image/png", "image/gif").contains(contentType);
+    }
+
+}
