9d9f902171a459648272011cfa5b253f4819d2ba
jw <rlawodnjs353@naver.com>
2025-02-07 17:24:45 +0900

    refactor(#54): MemberController 문서수정

--- src/main/java/friendy/community/domain/member/controller/SpringDocMemberController.java
+++ src/main/java/friendy/community/domain/member/controller/SpringDocMemberController.java
@@ -1,47 +1,43 @@
 package friendy.community.domain.member.controller;
 
-import friendy.community.domain.member.dto.request.MemberSignUpRequest;
 import friendy.community.domain.member.dto.request.PasswordRequest;
+import friendy.community.domain.member.dto.request.MemberSignUpRequest;
 import friendy.community.global.swagger.error.ApiErrorResponse;
 import friendy.community.global.swagger.error.ErrorCase;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "회원 API", description = "회원 API")
 public interface SpringDocMemberController {
     @Operation(summary = "회원가입")
     @ApiResponse(responseCode = "201", description = "회원가입 성공")
     @ApiErrorResponse(status = HttpStatus.BAD_REQUEST, instance = "/signup", errorCases = {
-            @ErrorCase(description = "이메일 입력 없음", exampleMessage = "이메일이 입력되지 않았습니다."),
-            @ErrorCase(description = "이메일 형식 오류", exampleMessage = "이메일 형식으로 입력해주세요."),
-            @ErrorCase(description = "닉네임 입력 없음", exampleMessage = "닉네임이 입력되지 않았습니다."),
-            @ErrorCase(description = "닉네임 글자수 오류", exampleMessage = "닉네임은 2~20자 사이로 입력해주세요."),
-            @ErrorCase(description = "비밀번호 입력 없음", exampleMessage = "비밀번호가 입력되지 않았습니다."),
-            @ErrorCase(description = "비밀번호 형식 오류", exampleMessage = "숫자, 영문자, 특수문자(~!@#$%^&*?)를 포함해야 합니다."),
-            @ErrorCase(description = "비밀번호 글자수 오류", exampleMessage = "비밀번호는 8~16자 사이로 입력해주세요."),
-            @ErrorCase(description = "생년월일 입력 없음", exampleMessage = "생년월일이 입력되지 않았습니다."),
-            @ErrorCase(description = "지원되지 않는 파일 확장자", exampleMessage = "지원되지 않는 파일 확장자입니다."),
-            @ErrorCase(description = "파일 크기가 허용된 범위 넘음", exampleMessage = "파일 크기가 허용된 범위를 초과했습니다.")
-
+        @ErrorCase(description = "이메일 입력 없음", exampleMessage = "이메일이 입력되지 않았습니다."),
+        @ErrorCase(description = "이메일 형식 오류", exampleMessage = "이메일 형식으로 입력해주세요."),
+        @ErrorCase(description = "닉네임 입력 없음", exampleMessage = "닉네임이 입력되지 않았습니다."),
+        @ErrorCase(description = "닉네임 글자수 오류", exampleMessage = "닉네임은 2~20자 사이로 입력해주세요."),
+        @ErrorCase(description = "비밀번호 입력 없음", exampleMessage = "비밀번호가 입력되지 않았습니다."),
+        @ErrorCase(description = "비밀번호 형식 오류", exampleMessage = "숫자, 영문자, 특수문자(~!@#$%^&*?)를 포함해야 합니다."),
+        @ErrorCase(description = "비밀번호 글자수 오류", exampleMessage = "비밀번호는 8~16자 사이로 입력해주세요."),
+        @ErrorCase(description = "생년월일 입력 없음", exampleMessage = "생년월일이 입력되지 않았습니다."),
     })
     @ApiErrorResponse(status = HttpStatus.CONFLICT, instance = "/signup", errorCases = {
-            @ErrorCase(description = "이메일 중복", exampleMessage = "이미 가입된 이메일입니다."),
-            @ErrorCase(description = "닉네임 중복", exampleMessage = "닉네임이 이미 존재합니다."),
+        @ErrorCase(description = "이메일 중복", exampleMessage = "이미 가입된 이메일입니다."),
+        @ErrorCase(description = "닉네임 중복", exampleMessage = "닉네임이 이미 존재합니다."),
     })
-    ResponseEntity<Void> signUp(
-        @RequestPart(value = "request") MemberSignUpRequest request,
-        @RequestPart(value = "image", required = false) MultipartFile multipartFile
-    );
+    ResponseEntity<Void> signUp(MemberSignUpRequest request);
 
     @Operation(summary = "비밀번호 변경")
     @ApiResponse(responseCode = "200", description = "비밀번호 변경 성공")
     @ApiErrorResponse(status = HttpStatus.UNAUTHORIZED, instance = "/auth/password", errorCases = {
-            @ErrorCase(description = "이메일 불일치", exampleMessage = "해당 이메일의 회원이 존재하지 않습니다.")
+        @ErrorCase(description = "이메일 불일치", exampleMessage = "해당 이메일의 회원이 존재하지 않습니다.")
     })
     ResponseEntity<Void> password(PasswordRequest passwordRequest);
 }
