5974e9bef86a3749f57f205581ee1abb81a392f6
Seio924 <seio9241@gmail.com>
2025-02-08 20:21:37 +0900

    feat(#59): 프로필 조회 엔드포인트 구현

--- src/main/java/friendy/community/domain/member/controller/MemberController.java
+++ src/main/java/friendy/community/domain/member/controller/MemberController.java
@@ -2,13 +2,13 @@ package friendy.community.domain.member.controller;
 
 import friendy.community.domain.member.dto.request.PasswordRequest;
 import friendy.community.domain.member.dto.request.MemberSignUpRequest;
+import friendy.community.domain.member.dto.response.FindMemberResponse;
 import friendy.community.domain.member.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -30,5 +30,13 @@ public class MemberController implements SpringDocMemberController{
         memberService.resetPassword(passwordRequest);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/member/{memberId}")
+    public ResponseEntity<FindMemberResponse> findMember(
+            HttpServletRequest httpServletRequest,
+            @PathVariable Long memberId
+    ) {
+        return ResponseEntity.ok(memberService.getMember(httpServletRequest, memberId));
     }
 }
