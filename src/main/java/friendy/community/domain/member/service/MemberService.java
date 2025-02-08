b7b0ff8d17e4762118acb03a724262d37e0631e1
Seio924 <seio9241@gmail.com>
2025-02-08 19:52:24 +0900

    feat(#59): 프로필 조회 로직 구현

--- src/main/java/friendy/community/domain/member/service/MemberService.java
+++ src/main/java/friendy/community/domain/member/service/MemberService.java
@@ -1,14 +1,18 @@
 package friendy.community.domain.member.service;
 
+import friendy.community.domain.auth.jwt.JwtTokenExtractor;
+import friendy.community.domain.auth.jwt.JwtTokenProvider;
 import friendy.community.domain.member.dto.request.PasswordRequest;
 import friendy.community.domain.auth.service.AuthService;
 import friendy.community.domain.member.dto.request.MemberSignUpRequest;
+import friendy.community.domain.member.dto.response.FindMemberResponse;
 import friendy.community.domain.member.encryption.PasswordEncryptor;
 import friendy.community.domain.member.encryption.SaltGenerator;
 import friendy.community.domain.member.model.Member;
 import friendy.community.domain.member.repository.MemberRepository;
 import friendy.community.global.exception.ErrorCode;
 import friendy.community.global.exception.FriendyException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +25,8 @@ public class MemberService {
     private final MemberRepository  memberRepository;
     private final SaltGenerator saltGenerator;
     private final PasswordEncryptor passwordEncryptor;
+    private final JwtTokenExtractor jwtTokenExtractor;
+    private final JwtTokenProvider jwtTokenProvider;
     private final AuthService authService;
 
     public Long signUp(MemberSignUpRequest request) {
@@ -43,6 +49,16 @@ public class MemberService {
         memberRepository.save(member);
     }
 
+    public FindMemberResponse getMember(HttpServletRequest httpServletRequest, Long memberId) {
+        final String accessToken = jwtTokenExtractor.extractAccessToken(httpServletRequest);
+        final String email = jwtTokenProvider.extractEmailFromAccessToken(accessToken);
+
+        final Member member = findMemberById(memberId);
+        boolean isMe = isCurrentUser(member, email);
+
+        return FindMemberResponse.from(member, isMe);
+    }
+
     public void validateUniqueMemberAttributes(MemberSignUpRequest request) {
         assertUniqueEmail(request.email());
         assertUniqueName(request.nickname());
@@ -59,4 +75,14 @@ public class MemberService {
             throw new FriendyException(ErrorCode.DUPLICATE_NICKNAME, "닉네임이 이미 존재합니다.");
         }
     }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new FriendyException(ErrorCode.RESOURCE_NOT_FOUND, "존재하지 않는 회원입니다."));
+    }
+
+    private boolean isCurrentUser(Member member, String email) {
+        return member.getEmail().equals(email);
+    }
+
 }
